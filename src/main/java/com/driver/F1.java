c7840dfec19d0a49a9c2d91016197a5d7a098ce9
Abhishikt <abhishiktsinha333@gmail.com>
2023-07-15 17:44:26 +0530

    Second Commit : added getters to vehicle and car class

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -1,19 +1,15 @@
 package com.driver;
 
 public class F1 extends Car {
-    private int currentDirection;
-    private int currentSpeed;
 
     public F1(String name, boolean isManual) {
         //Use arbitrary values for parameters which are not mentioned
         super(name, 4, "Race Car", 0, 8, isManual, 1);
-        currentDirection = 0;
-        currentSpeed = 0;
 
     }
 
     public void accelerate(int rate){
-        int newSpeed  = currentSpeed + rate; //set the value of new speed by using currentSpeed and rate
+        int newSpeed  = getCurrentSpeed() + rate; //set the value of new speed by using currentSpeed and rate
         /**
          * speed 0: gear 1
          * speed 1-50: gear 1
@@ -42,9 +38,6 @@ public class F1 extends Car {
             if(newSpeed > 200 && newSpeed < 251) changeGear(5);
             if(newSpeed > 250) changeGear(6);
         }
-        currentSpeed = newSpeed;
     }
-    public int getCurrentDirection() {
-        return currentDirection;
-    }
+
 }
